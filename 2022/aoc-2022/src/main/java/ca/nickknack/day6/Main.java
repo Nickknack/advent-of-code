3dddb409e20de29221085649b6ad0bc7ba383c1b
Nickknack <nickknack17@gmail.com>
2022-12-06 12:51:01 -0600

    day6 solution 2

--- 2022/aoc-2022/src/main/java/ca/nickknack/day6/Main.java
+++ 2022/aoc-2022/src/main/java/ca/nickknack/day6/Main.java
@@ -3,12 +3,20 @@ package ca.nickknack.day6;
 public class Main {
 
     public static void main(String[] args) {
-        int uniqueAmount = 4;
+        int uniqueAmountPacket = 4;
+        int uniqueAmountMessage = 14;
         Cipher cipher = new CipherReader().readCipherFromFile("day6-input");
 
         for (int i = 0; i < cipher.getMessageLength(); i++) {
-            if (cipher.containsAllUniqueCharacters(i, uniqueAmount)) {
-                System.out.println(String.format("first marker after character %s", i + uniqueAmount));
+            if (cipher.containsAllUniqueCharacters(i, uniqueAmountPacket)) {
+                System.out.println(String.format("first marker after character %s", i + uniqueAmountPacket));
+                break;
+            }
+        }
+
+        for (int i = 0; i < cipher.getMessageLength(); i++) {
+            if (cipher.containsAllUniqueCharacters(i, uniqueAmountMessage)) {
+                System.out.println(String.format("first message after character %s", i + uniqueAmountMessage));
                 break;
             }
         }
