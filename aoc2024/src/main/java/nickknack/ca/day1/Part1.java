626fdc7c7234af40f040aa3cc762670187d2ce80
Nickknack <nickknack17@gmail.com>
2024-12-01 12:44:01 -0600

    Day 1 completed

---
+++ aoc2024/src/main/java/nickknack/ca/day1/Part1.java
@@ -0,0 +1,46 @@
+package nickknack.ca.day1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class Part1 {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+
+        try (Scanner scanner = new Scanner(Thread.currentThread().getContextClassLoader().getResourceAsStream("day1/input1.txt"))) {
+            while (scanner.hasNext()) {
+                list1.add(scanner.nextInt());
+                list2.add(scanner.nextInt());
+            }
+        }
+
+        CompletableFuture<List<Integer>> sortList1Future = CompletableFuture.supplyAsync(() -> sortList(list1));
+        CompletableFuture<List<Integer>> sortList2Future = CompletableFuture.supplyAsync(() -> sortList(list2));
+
+        CompletableFuture sortLists = CompletableFuture.allOf(sortList1Future, sortList2Future);
+
+        sortLists.get();
+
+        List<Integer> sortedList1 = sortList1Future.get();
+        List<Integer> sortedList2 = sortList2Future.get();
+
+        int totalDistance = 0;
+        for (int i = 0; i < sortedList1.size(); i++) {
+            totalDistance += Math.abs(sortedList1.get(i) - sortedList2.get(i));
+        }
+
+        System.out.println(totalDistance);
+    }
+
+    private static List<Integer> sortList(List<Integer> list) {
+        List<Integer> listToSort = new ArrayList<>(list);
+        Collections.sort(listToSort);
+
+        return listToSort;
+    }
+}
---
+++ aoc2024/src/main/java/nickknack/ca/day1/Part2.java
@@ -0,0 +1,26 @@
+package nickknack.ca.day1;
+
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+
+public class Part2 {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        List<Integer> list1 = new ArrayList<>();
+        Map<Integer, Integer> secondListCount = new HashMap<>();
+        try (Scanner scanner = new Scanner(Thread.currentThread().getContextClassLoader().getResourceAsStream("day1/input1.txt"))) {
+            while (scanner.hasNext()) {
+                list1.add(scanner.nextInt());
+                int list2Number = scanner.nextInt();
+                secondListCount.put(list2Number, secondListCount.getOrDefault(list2Number, 0) + 1);
+            }
+        }
+
+        int similarityScore = 0;
+        for (int i = 0; i < list1.size(); i++) {
+            similarityScore += list1.get(i) * secondListCount.getOrDefault(list1.get(i), 0);
+        }
+
+        System.out.println(similarityScore);
+    }
+
+}
