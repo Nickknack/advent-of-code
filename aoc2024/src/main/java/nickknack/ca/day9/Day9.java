189ca3afc720671a1447d15eb4bd4cdd3b934a19
Nickkack <nickknack17@gmail.com>
2024-12-11 08:48:14 -0600

    part 2

--- aoc2024/src/main/java/nickknack/ca/day9/Day9.java
+++ aoc2024/src/main/java/nickknack/ca/day9/Day9.java
@@ -3,6 +3,7 @@ package nickknack.ca.day9;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class Day9 {
@@ -11,12 +12,15 @@ public class Day9 {
 
 	private static final boolean TEST = false;
 
+	private static final long EMPTY_SPACE = -1L;
+
 	public static void main(String[] args) {
 		String input = readInput(TEST ? TEST_INPUT : INPUT);
 
 		File[] files = parseInput(input);
 
 		part1(files);
+		part2(parseInputAsArray(input));
 	}
 
 	private static void part1(File[] files) {
@@ -26,6 +30,15 @@ public class Day9 {
 		long checksum = calculateChecksum(files);
 
 		System.out.printf("Part 1 result: %s\n", checksum);
+	}
+
+	private static void part2(long[] input) {
+		System.out.println("Part 2 start");
+		compactWholeFiles(input);
+
+		long checksum = calculateChecksumOfArray(input);
+
+		System.out.printf("Part 2 result: %s\n", checksum);
 	}
 
 	private static String readInput(String fileLocation) {
@@ -48,6 +61,27 @@ public class Day9 {
 		return files.toArray(File[]::new);
 	}
 
+	private static long[] parseInputAsArray(String input) {
+		long id = 0L;
+		List<Long> result = new ArrayList<>();
+		for (int i = 0; i < input.length(); i += 2) {
+			int block = Integer.parseInt(String.valueOf(input.charAt(i)));
+			int freeSpace = i + 1 == input.length() ? 0 : Integer.parseInt(String.valueOf(input.charAt(i + 1)));
+
+			for (int j = 0; j < block + freeSpace; j++) {
+				if (j < block) {
+					result.add(id);
+				} else {
+					result.add(EMPTY_SPACE);
+				}
+			}
+			id++;
+		}
+		return result.stream().mapToLong(l -> l)
+				.toArray();
+
+	}
+
 	private static void compact(File[] files) {
 		int rightmostNonEmptyIndex = findRightmostNonEmpty(files, files.length - 1);
 		for (int i = 0; i < files.length && i < rightmostNonEmptyIndex; i++) {
@@ -60,6 +94,65 @@ public class Day9 {
 				nonFullFile.addValueToSpace(files[rightmostNonEmptyIndex].popValue());
 			}
 		}
+	}
+
+	private static void compactWholeFiles(long[] input) {
+		int rightmostIndex = getRightmostBlockIndex(input, input.length - 1);
+		Optional<Integer> fileStartIndex = getFileStartIndex(input, rightmostIndex);
+
+		while (fileStartIndex.isPresent() && fileStartIndex.get() > 0) {
+			int spaceNeeded = rightmostIndex - fileStartIndex.get() + 1;
+			Optional<Integer> freeSpaceIndex = getIndexOfSectionWithEnoughSpace(input, spaceNeeded, fileStartIndex.get());
+			if (freeSpaceIndex.isPresent()) {
+				long id = input[rightmostIndex];
+				for (int i = 0; i < spaceNeeded; i++) {
+					input[freeSpaceIndex.get() + i] = id;
+					input[fileStartIndex.get() + i] = EMPTY_SPACE;
+				}
+			}
+
+			rightmostIndex = getRightmostBlockIndex(input, fileStartIndex.get() - 1);
+			fileStartIndex = getFileStartIndex(input, rightmostIndex);
+
+		}
+	}
+
+	private static int getRightmostBlockIndex(long[] input, int indexStart) {
+		for (int i = indexStart; i > 0; i--) {
+			if (input[i] != EMPTY_SPACE) {
+				return i;
+			}
+		}
+
+		return 0;
+	}
+
+	private static Optional<Integer> getIndexOfSectionWithEnoughSpace(long[] input, int spaceNeeded, int endIndex) {
+		int continuousFreeSpace = 0;
+		for (int i = 0; i <= endIndex; i++) {
+			if (input[i] == EMPTY_SPACE) {
+				continuousFreeSpace++;
+				if (continuousFreeSpace >= spaceNeeded) {
+					return Optional.of(i + 1 - spaceNeeded);
+				}
+			} else {
+				continuousFreeSpace = 0;
+			}
+		}
+		return Optional.empty();
+	}
+
+	private static Optional<Integer> getFileStartIndex(long[] input, int fileEndIndex) {
+		long id = input[fileEndIndex];
+		int fileStartIndex = fileEndIndex;
+		while (id == input[fileStartIndex]) {
+			fileStartIndex--;
+			if (fileStartIndex < 0) {
+				return Optional.empty();
+			}
+		}
+
+		return Optional.of(++fileStartIndex);
 	}
 
 	private static int findRightmostNonEmpty(File[] files, int startingIndex) {
@@ -79,6 +172,18 @@ public class Day9 {
 			for (long block : file.getBlocks()) {
 				hashTotal += block * hashPosition;
 				hashPosition++;
+			}
+		}
+
+		return hashTotal;
+	}
+
+	private static long calculateChecksumOfArray(long[] blocks) {
+		long hashTotal = 0L;
+
+		for (int i = 0; i < blocks.length; i++) {
+			if (blocks[i] != EMPTY_SPACE) {
+				hashTotal += blocks[i] * i;
 			}
 		}
 
