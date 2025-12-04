2323cfd229e3e519720d3f5fae6cfd435dd5aae2
Nickknack <nickknack17@gmail.com>
2025-12-03 23:21:54 -0600

    part 1

--- aoc2025/src/main/java/nickknack/ca/day3/Day3.java
+++ aoc2025/src/main/java/nickknack/ca/day3/Day3.java
@@ -9,39 +9,47 @@ import java.util.concurrent.CompletableFuture;
 public class Day3 {
 	static void main() {
 		List<String> testData = FileReader.readFile(3, DayPart.TEST);
+        List<String> actualData = FileReader.readFile(3, DayPart.PART_1);
 
 		CompletableFuture<Void> testPart1 = CompletableFuture.supplyAsync(() -> day3(testData))
 				.thenAccept(result -> System.out.printf("Part 1 Test: %s\n\n", result));
+
+        CompletableFuture<Void> part1 = CompletableFuture.supplyAsync(() -> day3(actualData))
+                .thenAccept(result -> System.out.printf("Part 1: %s\n\n", result));
+
+        CompletableFuture.allOf(testPart1, part1).join();
 	}
 
 	private static long day3(List<String> data) {
-
+        return data.stream()
+                .map(Day3::getJoltageForBank)
+                .peek(jolt -> System.out.println(jolt))
+                .reduce(0L, Long::sum);
 	}
 
 	private static long getJoltageForBank(String bank) {
-		long currentLeft = 0;
-		long currentRight = 0;
+		int currentLeft = 0;
+        int currentRight = 0;
 		for (int i = 0; i < bank.length() - 1; i++) {
-			long leftVal = Long.valueOf(bank.charAt(i));
+            int leftVal = Character.getNumericValue(bank.charAt(i));
 			if (leftVal > currentLeft) {
 				currentLeft = leftVal;
-				currentRight = Long.valueOf(bank.charAt(i + 1));
+                currentRight = 0;
 			}
 
-			for (int j = i + 2; j < bank.length(); j++) {
-				long rightVal = Long.valueOf(bank.charAt(j));
+			for (int j = i + 1; j < bank.length(); j++) {
+                int rightVal = Character.getNumericValue(bank.charAt(j));
 
-				if (j < bank.length() - 2 && rightVal > currentLeft) {
+				if (j < bank.length() - 1 && rightVal > currentLeft) {
 					currentLeft = rightVal;
-					currentRight = Long.valueOf(bank.charAt(j + 1));
-					i = j + 1;
-					break;
-				}
-
-				if (rightVal > currentRight) {
+                    currentRight = 0;
+                    i = j;
+				} else if (rightVal > currentRight) {
 					currentRight = rightVal;
 				}
 			}
 		}
+        return currentLeft * 10L + currentRight;
 	}
 }
+//17638
