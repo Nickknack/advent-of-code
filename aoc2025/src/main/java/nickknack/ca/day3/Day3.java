3bd4e468aa7a4e2c9a20347b40fb295db1eaa03d
Nickknack <nickknack17@gmail.com>
2025-12-04 00:16:16 -0600

    day 3 cleanup

--- aoc2025/src/main/java/nickknack/ca/day3/Day3.java
+++ aoc2025/src/main/java/nickknack/ca/day3/Day3.java
@@ -7,70 +7,43 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class Day3 {
-	static void main() {
+    private static final int BATTERY_AMOUNT_PART_1 = 2;
+    private static final int BATTERY_AMOUNT_PART_2 = 12;
+
+
+    static void main() {
 		List<String> testData = FileReader.readFile(3, DayPart.TEST);
         List<String> actualData = FileReader.readFile(3, DayPart.PART_1);
 
-//		CompletableFuture<Void> testPart1 = CompletableFuture.supplyAsync(() -> part1(testData))
-//				.thenAccept(result -> System.out.printf("Part 1 Test: %s\n\n", result));
-//
-//        CompletableFuture<Void> part1 = CompletableFuture.supplyAsync(() -> part1(actualData))
-//                .thenAccept(result -> System.out.printf("Part 1: %s\n\n", result));
+		CompletableFuture<Void> testPart1 = CompletableFuture.supplyAsync(() -> calculateTotalVoltage(testData, BATTERY_AMOUNT_PART_1))
+				.thenAccept(result -> System.out.printf("Part 1 Test: %s\n\n", result));
 
-        CompletableFuture<Void> part2Test = CompletableFuture.supplyAsync(() -> part2(testData))
+        CompletableFuture<Void> part1 = CompletableFuture.supplyAsync(() -> calculateTotalVoltage(actualData, BATTERY_AMOUNT_PART_1))
+                .thenAccept(result -> System.out.printf("Part 1: %s\n\n", result));
+
+        CompletableFuture<Void> part2Test = CompletableFuture.supplyAsync(() -> calculateTotalVoltage(testData, BATTERY_AMOUNT_PART_2))
                 .thenAccept(result -> System.out.printf("Part 2 Test: %s\n\n", result));
 
-        CompletableFuture<Void> part2 = CompletableFuture.supplyAsync(() -> part2(actualData))
+        CompletableFuture<Void> part2 = CompletableFuture.supplyAsync(() -> calculateTotalVoltage(actualData, BATTERY_AMOUNT_PART_2))
                 .thenAccept(result -> System.out.printf("Part 2: %s\n\n", result));
 
-        CompletableFuture.allOf(part2Test, part2).join();
+        CompletableFuture.allOf(testPart1, part1, part2Test, part2).join();
 	}
 
-	private static long part1(List<String> data) {
+    private static long calculateTotalVoltage(List<String> data, int batteryAmount) {
         return data.stream()
-                .map(Day3::part1)
-                .reduce(0L, Long::sum);
-	}
-
-    private static long part2(List<String> data) {
-        return data.stream()
-                .map(line -> part2(line, 12))
-                .peek(jolt -> System.out.println(jolt))
+                .map(line -> getVoltageForBank(line, batteryAmount))
                 .reduce(0L, Long::sum);
     }
 
-	private static long part1(String bank) {
-		int currentLeft = 0;
-        int currentRight = 0;
-		for (int i = 0; i < bank.length() - 1; i++) {
-            int leftVal = Character.getNumericValue(bank.charAt(i));
-			if (leftVal > currentLeft) {
-				currentLeft = leftVal;
-                currentRight = 0;
-			}
-
-			for (int j = i + 1; j < bank.length(); j++) {
-                int rightVal = Character.getNumericValue(bank.charAt(j));
-
-				if (j < bank.length() - 1 && rightVal > currentLeft) {
-					currentLeft = rightVal;
-                    currentRight = 0;
-                    i = j;
-				} else if (rightVal > currentRight) {
-					currentRight = rightVal;
-				}
-			}
-		}
-        return currentLeft * 10L + currentRight;
-	}
-
-    private static long part2(String bank, int size) {
+    private static long getVoltageForBank(String bank, int batteryAmount) {
         StringBuilder batteryBuilder = new StringBuilder();
-        int lastIndex = -1;
-        for (int i = 0; i < size; i++) {
-            int index = getLargestBatteryIndex(bank.substring(lastIndex + 1, bank.length() + 1 - size + i));
-            lastIndex += index + 1;
-            batteryBuilder.append(bank.charAt(lastIndex));
+        int start = 0;
+        int end = bank.length() + 1 - batteryAmount;
+        for (int i = 0; i < batteryAmount; i++) {
+            int index = getLargestBatteryIndex(bank.substring(start, end + i));
+            batteryBuilder.append(bank.charAt(start + index));
+            start += index + 1;
         }
 
         return Long.valueOf(batteryBuilder.toString());
